f80133e80916ef3effbd5e95c1930406bce9da34
tw4706 <towajobseach@gmail.com>
2025-10-21 17:47:54 +0900

    up

---
+++ VillageMonster/Game.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// ゲーム全体で使用する変数
+namespace Game
+{
+	// 画面情報
+	constexpr int kScreenWidth = 1280;
+	constexpr int kScreenHeight = 720;
+	constexpr int kColorBitNum = 32;
+}
---
+++ VillageMonster/main.cpp
@@ -0,0 +1,52 @@
+#include "DxLib.h"
+#include"Game.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// ウインドウモード設定
+	ChangeWindowMode(true);
+	// ウインドウのタイトル変更
+	SetMainWindowText("ゲーム名");
+	// 画面のサイズ変更
+	SetGraphMode(Game::kScreenWidth, Game::kScreenHeight, Game::kColorBitNum);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	// 描画対象をバックバッファに変更
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() != -1)
+	{
+		// このフレームの開始時間を取得
+		LONGLONG start = GetNowHiPerformanceCount();
+
+		// 前のフレームに描画した内容をクリアする
+		ClearDrawScreen();
+
+		// ここにゲームの処理を書く
+
+		// escキーを押したらゲームを強制終了
+		if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+
+		// 描画した内容を画面に反映する
+		ScreenFlip();
+
+		// フレームレート60に固定
+		while (GetNowHiPerformanceCount() - start < 16667)
+		{
+
+		}
+	}
+
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
