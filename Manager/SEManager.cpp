1bb66328753a1f7c7371d85dbef30874282f9423
tw4706 <towajobseach@gmail.com>
2026-01-10 14:14:35 +0900

    SEクラス実装

--- Manager/SEManager.cpp
+++ Manager/SEManager.cpp
@@ -1,1 +1,27 @@
 #include "SEManager.h"
+#include<DxLib.h>
+
+void SEManager::Init()
+{
+	seHandles_[SE::Decide] = LoadSoundMem("data/BGM・SE/decide.wav");
+	seHandles_[SE::Cancel] = LoadSoundMem("data/BGM・SE/cancel.wav");
+	seHandles_[SE::Select] = LoadSoundMem("data/BGM・SE/select.wav");
+	seHandles_[SE::Shot] = LoadSoundMem("data/BGM・SE/Shot.wav");
+	seHandles_[SE::Hit] = LoadSoundMem("data/BGM・SE/Hit.wav");
+	seHandles_[SE::Damage] = LoadSoundMem("data/BGM・SE/Damage.wav");
+	seHandles_[SE::WeaponChange] = LoadSoundMem("data/BGM・SE/WeaponChange.wav");
+}
+
+void SEManager::PlaySE(SE se)
+{
+	int handle = seHandles_[se];
+
+	ChangeVolumeSoundMem(volume_, handle);
+
+	PlaySoundMem(handle, DX_PLAYTYPE_BACK);
+}
+
+void SEManager::SetVolume(int volume)
+{
+	volume_ = volume;
+}
--- SEManager.h
+++ SEManager.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <map>
+
+enum class SE
+{
+	Decide,
+	Cancel,
+	Select,
+	Shot,
+	Hit,
+	Damage,
+	WeaponChange,
+};
+
 class SEManager
 {
+public:
+	void Init();
+	void PlaySE(SE se);
+	void SetVolume(int volume);
+
+private:
+	std::map<SE, int>seHandles_;
+	int volume_ = 200;
 };
 
